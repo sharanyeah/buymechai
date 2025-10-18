417b07c17db065cdca729b21f653f63358b37f12
sharanyeah <kallemsharanyareddy21@gmail.com>
2025-10-18 21:09:47 +0530

    Add 'tw-animate-css' plugin to PostCSS config

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,7 @@ export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
+    'tw-animate-css': {}, // if you use it
   },
+  from: undefined, // prevents the PostCSS `from` warning
 }
